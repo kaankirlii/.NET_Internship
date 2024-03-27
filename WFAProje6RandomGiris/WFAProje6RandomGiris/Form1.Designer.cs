88b6e7bb1ab03a40916fd414bfd30e68af14440b
Kaan Kırlı <73934843+kaankirlii@users.noreply.github.com>
2024-03-27 17:24:32 +0300

    Add files via upload

---
+++ WFAProje6RandomGiris/WFAProje6RandomGiris/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WFAProje6RandomGiris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.random = new System.Windows.Forms.Label();
+            this.clickButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(53, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(560, 74);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lütfen aşağıda görmüş olduğunuz karakterleri yazı alanına yazınız..";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+     
+            this.label1.Layout += new System.Windows.Forms.LayoutEventHandler(this.label1_Layout);
+            // 
+            // random
+            // 
+            this.random.AutoSize = true;
+            this.random.Location = new System.Drawing.Point(224, 117);
+            this.random.Name = "random";
+            this.random.Size = new System.Drawing.Size(0, 13);
+            this.random.TabIndex = 1;
+            // 
+            // clickButton
+            // 
+            this.clickButton.Location = new System.Drawing.Point(285, 150);
+            this.clickButton.Name = "clickButton";
+            this.clickButton.Size = new System.Drawing.Size(107, 37);
+            this.clickButton.TabIndex = 2;
+            this.clickButton.Text = "Giriş Yap";
+            this.clickButton.UseVisualStyleBackColor = true;
+            this.clickButton.Click += new System.EventHandler(this.clickButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(124, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+           
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(124, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kalan Hakkınız :";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(127, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 6;
+            // 
+            // number
+            // 
+            this.number.AutoSize = true;
+            this.number.Location = new System.Drawing.Point(307, 234);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(0, 13);
+            this.number.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 310);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.clickButton);
+            this.Controls.Add(this.random);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label random;
+        private System.Windows.Forms.Button clickButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label number;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WFAProje6RandomGiris/WFAProje6RandomGiris/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAProje6RandomGiris
+{
+    public partial class Form1 : Form
+    {
+
+       
+        int h =3 ; //hak sayısı 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void label1_Layout(object sender, LayoutEventArgs e)
+        {
+            Console.WriteLine("Lütfen aşağıda görmüş olduğunuz karakterleri yazı alanına yazınız.."); //label yazısı 
+        }
+
+        private void clickButton_Click(object sender, EventArgs e)
+        {
+                if(textBox2.Text == label3.Text)   //textbox labekl'a eşitse 
+            {
+                MessageBox.Show("Başarıyla girilmiştir!");
+                Close();  //uygulamayı kapat 
+           
+            }   else
+            {
+                Random r = new Random();       //random belirlenir
+                label3.Text = r.Next(10000, 99999) + ""; //label'da random sayı yazılır 
+
+                if (h > 0)    //h > 0  hak sayısı 1 azalır
+                {
+                    MessageBox.Show("Yanlış Sayı"); //yanlış sayı mesajı
+                    h--; // hak sayısı 1 azalır 
+                    label4.Text = h.ToString();   //hak sayısı string'de "Kalan Hakkınız" 'ın yanında gözükür. 
+                }
+                if (h == 0) // hak sayısı bittiyse
+                {
+                    MessageBox.Show("Hakkınız Doldu!");   // hak doldu
+                    clickButton.Enabled = false;   //buton kapandı
+                }
+
+            }
+
+        
+            }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = h.ToString();  //hak sayısı yazılır
+            Random r = new Random();       //random belirlenir
+            label3.Text = r.Next(10000, 99999) + "";   //label'da random sayı yazılır
+            
+        }
+
+      
+    }
+}
