2fe21b2da19b5a1493d777d641007327e686ead4
Kaan Kırlı <73934843+kaankirlii@users.noreply.github.com>
2024-03-27 17:30:00 +0300

    Add files via upload

---
+++ WFAProje11Barbut/WFAProje11Barbut/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WFAProje11Barbut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.skor1 = new System.Windows.Forms.Label();
+            this.skor2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Location = new System.Drawing.Point(92, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "oyuncu1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.ForestGreen;
+            this.button2.Location = new System.Drawing.Point(387, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "oyuncu2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // skor1
+            // 
+            this.skor1.Location = new System.Drawing.Point(107, 83);
+            this.skor1.Name = "skor1";
+            this.skor1.Size = new System.Drawing.Size(160, 36);
+            this.skor1.TabIndex = 2;
+            this.skor1.Text = "Skor 1";
+            this.skor1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // skor2
+            // 
+            this.skor2.Location = new System.Drawing.Point(384, 83);
+            this.skor2.Name = "skor2";
+            this.skor2.Size = new System.Drawing.Size(150, 36);
+            this.skor2.TabIndex = 3;
+            this.skor2.Text = "Skor 2";
+            this.skor2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 394);
+            this.Controls.Add(this.skor2);
+            this.Controls.Add(this.skor1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label skor1;
+        private System.Windows.Forms.Label skor2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WFAProje11Barbut/WFAProje11Barbut/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAProje11Barbut
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //int sayi1;
+        //int sayi2;
+
+        Random rand = new Random();  // Random belirlenir
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+                try
+            {
+                 timer1.Start();
+                timer1.Interval = 3000;
+
+                skor1.Text = rand.Next(1, 7) + ""; /* Her butona basılınca 1 ile 7 arasındaki (7 dahil değil) sayılardan biri rastgele çıkar 
+                                                ve string'e dönüşerek label'da görünür */
+                skor2.Text = rand.Next(1, 7) + "";
+
+                timer1.Stop();
+            } catch
+            {
+
+            }
+
+           // MessageBox.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+             timer1.Start();
+                timer1.Interval = 3000;
+
+                //Random rand2 = new Random(); //Random belirlenir  
+                skor2.Text = rand.Next(1, 7) +""; /* Her butona basılınca 1 ile 7 arasındaki sayılardan (7 dahil değil) biri rastgele çıkar 
+                                                ve string'e dönüşerek label'da görünür */
+
+            skor1.Text = rand.Next(1, 7) + "";
+                timer1.Stop();
+
+            } catch
+            {
+
+
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+            timer1.Interval = 3000;
+            timer1.Stop();
+
+
+
+            //if (sayi1 > sayi2)
+            //{
+            //    MessageBox.Show("1. Oyuncu Galip!");
+            //}else if(sayi2> sayi1)
+            //{
+            //    MessageBox.Show("2. Oyuncu Galip!");
+            //}else if(sayi1 == sayi2)
+            //{
+            //   // MessageBox.Show("Berabere!");
+            //}
+            
+
+            try
+            {
+             
+                
+
+            }catch
+            {
+                MessageBox.Show("Fazla girdiniz");
+            }
+
+
+ 
+        }
+    }
+}
+
+
+//
+//label4.Text = h.ToString();  //hak sayısı yazılır
+  //          Random r = new Random();       //random belirlenir
+//label3.Text = r.Next(10000, 99999) + "";   //label'da random sayı yazılır
