582152c8c6dc61d64e42ded4873625ee21322c2c
Kaan Kırlı <73934843+kaankirlii@users.noreply.github.com>
2024-03-27 17:23:06 +0300

    Add files via upload

---
+++ WFAProje4/WFAProje4/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WFAProje4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Operator = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+        
+            // 
+            // Operator
+            // 
+            this.Operator.Location = new System.Drawing.Point(157, 55);
+            this.Operator.Name = "Operator";
+            this.Operator.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.Operator.Size = new System.Drawing.Size(100, 20);
+            this.Operator.TabIndex = 1;
+          
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Operation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Operator);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Operator;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WFAProje4/WFAProje4/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WFAProje4
+{
+    public partial class Form1 : Form
+    {
+        bool a;
+
+        int result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Make an operation 
+
+            if(a == true)
+            {
+                if(Operator.Text == "+")
+                {
+                    result += Convert.ToInt32(textBox1.Text);
+                    textBox1.Text = result.ToString();
+                }else if(Operator.Text == "-")
+                {
+                    result -= Convert.ToInt32(textBox1.Text);
+                    textBox1.Text = result.ToString();
+                }else if(Operator.Text == "*")
+                {
+                    result *= Convert.ToInt32(textBox1.Text);
+                    textBox1.Text = result.ToString();
+                }else if (Operator.Text == "/")
+                {
+                    result /= Convert.ToInt32(textBox1.Text);
+                    textBox1.Text = result.ToString();
+                }else
+                {
+                    MessageBox.Show("Please enter an operator!");
+                }
+
+
+            }else
+            {
+                Convert.ToInt32(textBox1.Text);
+                a = true;
+            }
+
+          
+
+  
+  
+
+        }
+    }
+}
+
