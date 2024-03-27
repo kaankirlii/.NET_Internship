de642060212199f18ff6a66f48b05010471c04dc
Kaan Kırlı <73934843+kaankirlii@users.noreply.github.com>
2024-03-27 17:27:16 +0300

    Add files via upload

---
+++ WFAProje10TryCatchYapisi/WFAProje10TryCatchYapisi/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAProje10TryCatchYapisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //int a = Convert.ToInt32(textBox1.Text);
+
+                //int b = Convert.ToInt32(textBox2.Text);
+
+                //int sonuc = a / b;
+
+              //  MessageBox.Show(sonuc.ToString());
+
+
+                //tek satırda yaz
+
+
+
+
+             // int sonuc = MessageBox.Show( (Convert.ToInt32(textBox1.Text) / (Convert.ToInt32(textBox2.Text) ));
+
+            } catch
+            {
+                
+
+            }
+
+
+        }
+    }
+}
